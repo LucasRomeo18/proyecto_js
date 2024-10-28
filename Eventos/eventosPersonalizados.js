11abe7aee22e2dafe9332aa8720e864ea317fca7
Lucas Econo <lucas.romeo18@gmail.com>
2024-10-28 09:28:23 -0300

    evento personalizado corrección

--- Eventos/eventosPersonalizados.js
+++ Eventos/eventosPersonalizados.js
@@ -1,2 +1,21 @@
 let audio = document.getElementById(`audio`);
 let listacanciones = document.getElementById(`listaCanciones`);
+
+
+//evento change
+
+listacanciones.addEventListener(`change`, cambiarCancion);
+
+function cambiarCancion(){
+    let cancionEleginda = listacanciones.value;
+    audio.src = cancionEleginda;
+    audio.play();
+    let evento = new CustomEvent(`cambioDeCancion`);
+    audio.dispatchEvent(evento);
+}
+
+audio.addEventListener("cambioDeCancion", mostrarCancion);
+
+function mostrarCancion(){
+    console.log(`Cancion Actual: ` + listacanciones.value);
+}
